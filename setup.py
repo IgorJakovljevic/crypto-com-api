61b9f2277fd77e953b2ae6c95790f00d21893f49
Igor Jakovljevic <igor.jakovljevic@outlook.com>
2019-12-09 19:46:59 +0100

    Changed Setup File

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-with open("README.md", "r", encoding='utf-8') as fh:
-    long_description = fh.read()
+# with open("README.md", "r", encoding='utf-8') as fh:
+#     long_description = fh.read()
 
 setup(
   name = 'cryptocomapi',         # How you named your package folder (MyLib)
   packages = ['cryptocomapi'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python Wrapper for Crypto.com API',   # Give a short description about your library
   # long_description=long_description,
@@ -14,7 +14,7 @@ setup(
   author = 'Igor Jakovljevic',                   # Type in your name
   author_email = 'igor.jakovljevic@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/IgorJakovljevic/crypto-com-api',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/IgorJakovljevic/crypto-com-api/archive/0.1-alpha.tar.gz',    
+  download_url = 'https://github.com/IgorJakovljevic/crypto-com-api/archive/v1.1.tar.gz',    
   keywords = ['Crypto-com', 'API', 'Python'],   # Keywords that define your package best
   install_requires=[           
           'requests'
